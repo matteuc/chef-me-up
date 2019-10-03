4f36927d5e94da7ec5e04c4f38e6d6dab78be5a1
matteuc <ymc006@ucsd.edu>
2019-10-02 21:11:17 -0700

    added main.handlebars layout, does not render correctly

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    var currPath = window.location.pathname;
+
+    switch (currPath) {
+        case "/":
+            $("#ingredients-tab").addClass("active");
+            break;
+        case "/recipes":
+            $("#recipes-tab").addClass("active");
+            break;
+        case "/favorites":
+            $("#favorites-tab").addClass("active");
+            break;
+    }
+
+
+    $(".nav-link").bind("click", function (event) {
+        event.preventDefault();
+        var clickedItem = $(this);
+        $(".nav-link").each(function () {
+            $(this).removeClass("active");
+        });
+        clickedItem.addClass("active");
+    });
+
+    $("[data-link]").click(function () {
+        window.location.href = $(this).attr("data-link");
+        return false;
+    });
+
+});
