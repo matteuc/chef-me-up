44f71c5c1743946515c0971363f859adfff21a59
Mitu Fikir <hannatefera2@gmail.com>
2019-10-05 11:11:18 -0700

    Modified schema and added ingredient read query

--- models/recipeIngredient.js
+++ models/recipeIngredient.js
@@ -1,33 +1,22 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable indent */
 module.exports = function(sequelize, DataTypes) {
-  var RecipeIngredient = sequelize.define("RecipeIngredient", {
-    unit: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    quantity: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    }
-  });
-  RecipeIngredient.associate = function(models) {
-    RecipeIngredient.belongsTo(models.Ingredient, {
-      foreignKey: {
-        allowNull: false,
-        as: "Ingredient"
-      }
+    var RecipeIngredient = sequelize.define("RecipeIngredient", {
+        unit: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        quantity: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        }
     });
-    RecipeIngredient.belongsTo(models.Recipe, {
-      foreignKey: {
-        allowNull: false,
-        as: "Recipe"
-      }
-    });
-  };
-  return RecipeIngredient;
+
+    return RecipeIngredient;
 };
