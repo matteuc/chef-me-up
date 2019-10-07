cf24584a2cd9cb9703e5cd2407fc4e4f6f3c1ed5
Matt <ymc006@ucsd.edu>
2019-10-07 10:01:06 -0700

    new recipe ingredient and instruction blocks adding to form

--- public/js/fridge.js
+++ public/js/fridge.js
@@ -55,7 +55,6 @@ $(document).ready(function () {
                 $("#sync-fridge-btn").removeClass("fa-spin");
 
                 var ingredientIDs = data;
-                console.log(ingredientIDs);
                 markIngredientElements(ingredientIDs);
 
             })
--- public/js/recipes.js
+++ public/js/recipes.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
-    var loginAlertRecipes = `<div class="text-center">\
-    <h5>To add your own recipes, please login:</h5>
-    <a href="/auth/google">\
-      <img class="img-fluid login-btn" src="images/login-google.png">\
-    </a>\
-</div>`;
+    var loginAlertRecipes = Handlebars.templates.loginAlert({ message: "To add your own recipes, please login:" });
+    var addRecipeForm = Handlebars.templates.addRecipeForm({});
+    var newIngredientList = $("#new-ingredient-list");
+    var ingredientIdx = 0;
+    var newInstructionList = $("#new-instruction-list");
+    var instructionIdx = 0;
+
+
     var USER_RECIPES_PATH;
     var ALL_RECIPES_PATH = "/api/recipes";
     var CUSTOM_RECIPES_PATH = "/api/custom/recipes";
@@ -22,37 +24,37 @@ $(document).ready(function () {
     function loadRecipes() {
         function renderRecipes(recipes) {
             recipesContent.empty();
-            if(recipes.length) {
+            if (recipes.length) {
                 $.each(recipes, function (recipe) {
-        
+
                     var recipePartial = Handlebars.templates.recipeItem(recipe);
                     recipesContent.append(recipePartial);
-        
+
                 })
             } else {
                 noRecipesMsg.show();
             }
         }
         // If user only want to return recipes that matches their ingredients
-        if(ingredientOnly) {
+        if (ingredientOnly) {
 
             if (userToken) {
                 $.get(USER_RECIPES_PATH, function (res) {
                     var recipes = res;
                     renderRecipes(recipes);
                 })
-    
+
             } else {
                 // Make an API request based on the locally stored ingredients list
                 var ingredients = localStorage.getItem("ingredients");
-                if(ingredients) {
+                if (ingredients) {
                     $.get(CUSTOM_RECIPES_PATH, ingredients, function (res) {
                         var recipes = res;
                         renderRecipes(recipes);
                     })
                 }
             }
-        } 
+        }
         // If user wants to return all recipes in the database
         else {
             $.get(ALL_RECIPES_PATH, function (data) {
@@ -72,15 +74,22 @@ $(document).ready(function () {
         e.preventDefault();
         if (userToken) {
             // TODO: Show Bootbox Modal to prompt recipe creation
-            var recipeInfo = {
+            bootbox.confirm({
+                message: addRecipeForm,
+                centerVertical: true,
+                closeButton: false,
+                callback: function (response) {
+                    var recipeInfo = {
 
-            };
-            $.ajax({
-                url: "/api/recipes",
-                type: "POST",
-                data: recipeInfo,
-                success: function (res) {
+                    };
+                    $.ajax({
+                        url: "/api/recipes",
+                        type: "POST",
+                        data: recipeInfo,
+                        success: function (res) {
 
+                        }
+                    })
                 }
             })
 
@@ -91,6 +100,24 @@ $(document).ready(function () {
                 closeButton: false
             })
         }
+    })
+
+    $(document).on("click", "#add-recipe-ingredient-btn", function(){
+        ingredientIdx++;
+        var newIngredientForm = Handlebars.templates.newRecipeIngredient({
+            idx: ingredientIdx
+        });
+        $("#new-ingredient-list").append(newIngredientForm);
+    })
+
+    $(document).on("click", "#add-recipe-instruction-btn", function(){
+        instructionIdx++;
+        var newInstructionForm = Handlebars.templates.newRecipeInstruction({
+            num: instructionIdx + 1,
+            idx: instructionIdx
+        });
+        $("#new-instruction-list").append(newInstructionForm);
+
     })
 
     // Load recipes when page first loads
