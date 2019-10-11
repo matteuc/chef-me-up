f8e1d8a554437194773d7c046fa97fb16b555922
Matt <ymc006@ucsd.edu>
2019-10-11 13:18:18 -0700

    added so you can go to the recipe page of the favorite

--- public/js/partialsCompiled.js
+++ public/js/partialsCompiled.js
@@ -22,9 +22,11 @@ templates['favorite'] = template({"compiler":[8,">= 4.3.0"],"main":function(cont
     + alias5(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"name","hash":{},"data":data}) : helper)))
     + "\">\r\n    <div class=\"d-flex justify-content-between align-items-center\">\r\n        <span class=\"font-weight-bold text-muted\">"
     + alias5(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</span>\r\n        <i data-id=\""
+    + "</span>\r\n        <div>\r\n            <i data-id=\""
     + alias5(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\" class=\"favorite-btn text-danger fas fa-heart\"></i>\r\n    </div>\r\n</li>";
+    + "\" class=\"favorite-btn text-danger fas fa-heart\"></i>\r\n            <i data-link=\"/recipes/"
+    + alias5(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"id","hash":{},"data":data}) : helper)))
+    + "\" class=\" ml-2 text-info fas fa-info-circle\"></i>\r\n        </div>\r\n    </div>\r\n</li>";
 },"useData":true});
 templates['ingredient'] = template({"1":function(container,depth0,helpers,partials,data) {
     var helper, alias1=container.propertyIsEnumerable, alias2=depth0 != null ? depth0 : (container.nullContext || {}), alias3=container.hooks.helperMissing, alias4="function", alias5=container.escapeExpression;
@@ -101,26 +103,32 @@ templates['recipeItem'] = template({"1":function(container,depth0,helpers,partia
     + container.escapeExpression(((helper = (helper = helpers.numMatches || (depth0 != null ? depth0.numMatches : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"numMatches","hash":{},"data":data}) : helper)))
     + "</span>&nbsp;<span class=\"text-muted\">matches</span>\r\n";
 },"3":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=container.propertyIsEnumerable;
+
+  return "      <i data-link=\"/recipes/"
+    + container.escapeExpression(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"id","hash":{},"data":data}) : helper)))
+    + "\" class=\" ml-2 text-info fas fa-info-circle\"></i>\r\n\r\n";
+},"5":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=container.propertyIsEnumerable, alias2=depth0 != null ? depth0 : (container.nullContext || {});
 
-  return "       <br>\r\n      <p class=\"font-italic\">You currently have <span class=\"text-success font-weight-bold\">"
+  return "      <br>\r\n      <p class=\"font-italic\">You currently have <span class=\"text-success font-weight-bold\">"
     + container.escapeExpression(((helper = (helper = helpers.numMatches || (depth0 != null ? depth0.numMatches : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(alias2,{"name":"numMatches","hash":{},"data":data}) : helper)))
-    + "</span> of this recipe's ingredients!</p>\r\n      <ul class=\"list-group\">\r\n"
-    + ((stack1 = helpers.each.call(alias2,(depth0 != null ? depth0.matches : depth0),{"name":"each","hash":{},"fn":container.program(4, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "</span> of\r\n        this recipe's ingredients!</p>\r\n      <ul class=\"list-group\">\r\n"
+    + ((stack1 = helpers.each.call(alias2,(depth0 != null ? depth0.matches : depth0),{"name":"each","hash":{},"fn":container.program(6, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "      </ul>\r\n";
-},"4":function(container,depth0,helpers,partials,data) {
+},"6":function(container,depth0,helpers,partials,data) {
     var helper, alias1=container.propertyIsEnumerable;
 
   return "        <li class=\"list-group-item\">"
     + container.escapeExpression(((helper = (helper = helpers.ingredientName || (depth0 != null ? depth0.ingredientName : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"ingredientName","hash":{},"data":data}) : helper)))
     + "</li>\r\n";
-},"6":function(container,depth0,helpers,partials,data) {
+},"8":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=container.propertyIsEnumerable, alias2=depth0 != null ? depth0 : (container.nullContext || {});
 
-  return "       <br>\r\n      <p class=\"font-italic\">You currently do not have <span class=\"text-danger font-weight-bold\">"
+  return "      <br>\r\n      <p class=\"font-italic\">You currently do not have <span class=\"text-danger font-weight-bold\">"
     + container.escapeExpression(((helper = (helper = helpers.numMissing || (depth0 != null ? depth0.numMissing : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(alias2,{"name":"numMissing","hash":{},"data":data}) : helper)))
-    + "</span> of this recipe's ingredients!</p>\r\n      <ul class=\"list-group\">\r\n"
-    + ((stack1 = helpers.each.call(alias2,(depth0 != null ? depth0.missing : depth0),{"name":"each","hash":{},"fn":container.program(4, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "</span>\r\n        of this recipe's ingredients!</p>\r\n      <ul class=\"list-group\">\r\n"
+    + ((stack1 = helpers.each.call(alias2,(depth0 != null ? depth0.missing : depth0),{"name":"each","hash":{},"fn":container.program(6, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "      </ul>\r\n";
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=container.propertyIsEnumerable, alias2=depth0 != null ? depth0 : (container.nullContext || {}), alias3=container.hooks.helperMissing, alias4="function", alias5=container.escapeExpression;
@@ -134,16 +142,16 @@ templates['recipeItem'] = template({"1":function(container,depth0,helpers,partia
     + "\">\r\n    <span class=\"mb-0\">\r\n      "
     + alias5(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"name","hash":{},"data":data}) : helper)))
     + "\r\n    </span>\r\n    <div>\r\n"
-    + ((stack1 = helpers["if"].call(alias2,(depth0 != null ? depth0.numMatches : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias2,(depth0 != null ? depth0.numMatches : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.program(3, data, 0),"data":data})) != null ? stack1 : "")
     + "    </div>\r\n  </div>\r\n\r\n  <div id=\"collapse-"
     + alias5(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"id","hash":{},"data":data}) : helper)))
     + "\" class=\"collapse\" aria-labelledby=\"heading-"
     + alias5(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\">\r\n    <div class=\"card-body text-center\">\r\n       <button data-link=\"/recipes/"
+    + "\">\r\n    <div class=\"card-body text-center\">\r\n      <button data-link=\"/recipes/"
     + alias5(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\" type=\"button\" class=\"btn btn-info btn-block\">View recipe&nbsp;&nbsp;<i class=\"fas fa-info-circle fa-sm\"></i></button>\r\n"
-    + ((stack1 = helpers["if"].call(alias2,(depth0 != null ? depth0.numMatches : depth0),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + ((stack1 = helpers["if"].call(alias2,(depth0 != null ? depth0.numMissing : depth0),{"name":"if","hash":{},"fn":container.program(6, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "\" type=\"button\" class=\"btn btn-info btn-block\">View recipe&nbsp;&nbsp;<i\r\n          class=\"fas fa-info-circle fa-sm\"></i></button>\r\n"
+    + ((stack1 = helpers["if"].call(alias2,(depth0 != null ? depth0.numMatches : depth0),{"name":"if","hash":{},"fn":container.program(5, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(alias2,(depth0 != null ? depth0.numMissing : depth0),{"name":"if","hash":{},"fn":container.program(8, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "\r\n    </div>\r\n  </div>\r\n</div>";
 },"useData":true});
 templates['searchbar'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
