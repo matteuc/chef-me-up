f1ddbf8f04f5636ff7de4e0aa975ccea24946177
matteuc <ymc006@ucsd.edu>
2019-10-10 23:49:21 -0700

    removed creation of 1K ingredients

--- cmu-server.js
+++ cmu-server.js
@@ -5,7 +5,7 @@ var exphbs = require("express-handlebars");
 var passport = require("passport");
 var auth = require("./config/auth");
 var db = require("./models");
-var ingredients = require("./ingredients.json");
+// var ingredients = require("./ingredients.json");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -32,36 +32,36 @@ app.engine(
   exphbs({
     defaultLayout: "main"
   })
-  );
-  app.set("view engine", "handlebars");
+);
+app.set("view engine", "handlebars");
   
-  // Routes
-  require("./routes/apiRoutes")(app);
-  require("./routes/htmlRoutes")(app);
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
   
-  var syncOptions = { force: false };
+var syncOptions = { force: false };
   
-  // If running a test, set syncOptions.force to true
-  // clearing the `testdb`
-  if (process.env.NODE_ENV === "test") {
-    syncOptions.force = true;
-  }
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
+  syncOptions.force = true;
+}
   
-  // Starting the server, syncing our models ------------------------------------/
-  db.sequelize.sync(syncOptions).then(function() {
-    app.listen(PORT, function() {
-      db.Ingredient.bulkCreate(ingredients).then(function(res) {
-        if (res) {
-          console.log(true);
-        }
-      });
-      console.log(
-        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-        PORT,
-        PORT
-        );
-      });
-    });
+// Starting the server, syncing our models ------------------------------------/
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    // db.Ingredient.bulkCreate(ingredients).then(function(res) {
+    //   if (res) {
+    //     console.log(true);
+    //   }
+    // });
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
     
-    module.exports = app;
+module.exports = app;
     
